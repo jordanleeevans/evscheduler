5e55474283c5bfd042e276b7c518b267e8659949
Jordan Evans <jordanleeevans@hotmail.com>
2026-04-30 07:47:46 +1200

    feat(frontent): initialize frontent project with Next.js, TypeScript, and Tailwind CSS
    - Add package.json with dependencies and scripts for Next.js and ESLint
    - Create postcss.config.mjs for Tailwind CSS integration
    - Set up tsconfig.json for TypeScript configuration with strict settings

---
+++ frontent/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import {Sulphur_Point} from "next/font/google";
+import "./globals.css";
+
+const sulphurPoint = Sulphur_Point({
+  variable: "--font-sulphur-point",
+  subsets: ["latin"],
+  weight: "300"
+});
+
+export const metadata: Metadata = {
+  title: "EV Scheduler",
+  description: "A simple app to schedule electric vehicle charging sessions based on time-of-use tariffs.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${sulphurPoint.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
